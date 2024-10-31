c9cf4e1ae536ff5feeefbb1ff0381a7e4665af60
KatG33 <Ekgr2021@gmail.com>
2024-10-31 17:35:07 +0800

    Trying to set Up URL Routing with Django

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# imports admin module to manage data through admin interface
 from django.contrib import admin
-from django.urls import path
-
+# 'path' permits to define URL routes, 
+# 'include' allows to include URL patterns from other components, 
+#       like 'flashcards' into the main folder 'backend'
+from django.urls import include, path
+# 'urlpatterns' - list that stores all URLs. 
+#       Each item in this list defines a URL pattern and
+#       specifies which view or URL configuration should handle it
 urlpatterns = [
+    # To see admin interface, visit http://127.0.0.1:8000/admin/ in a browser
     path('admin/', admin.site.urls),
+    # To look inside 'flashcards' component's configuration, 
+    #       add flashcards/ at the end of a URL: http://127.0.0.1:8000/flashcards/
+    path('flashcards/', include('flashcards.urls')),
 ]
--- backend/flashcards/urls.py
+++ backend/flashcards/urls.py
@@ -1,9 +1,13 @@
+# This module allows to define URL pattern in Django application 
 from django.urls import path
+# This module contains logic for handling requests and returning responses for the app Flashcards
 from . import views
-
+# The list below contains URL patters Django project needs to recognise.
+# Each URL provides an address to view a function that manages the request
 urlpatterns = [
-    path('', views.flashcard_list, name='flashcard_list'),  # Map root URL to the view
+# '' - means that this pattern corresponds to the root URL of the program: http://127.0.0.1:8000/flashcards/
+# 'views.flashcard_list' - indicates that when user visits the URL,
+#   Django supposed to call the flashcard_list function defined in the views module and processes request.
+# name='flashcard_list' - assigns a name to this URL pattern
+    path('', views.flashcard_list, name='flashcard_list'), 
 ]
-
-
-# 
